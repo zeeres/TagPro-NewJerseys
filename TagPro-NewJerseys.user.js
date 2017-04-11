c56e90753cab50935c995ce88e304144fa1c70fc
zeeres <zeeresgit@server3.digifive.de>
2017-04-11 19:19:49 +0200

    added ELTP_S9 and ENLTP_S2 jerseys, removed jquery cookie plugin

--- TagPro-NewJerseys.user.js
+++ TagPro-NewJerseys.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TagPro NewJerseys
-// @version      1.1.0
+// @version      1.1.1
 // @description  Set and change ball jerseys directly from the group page
 // @author       zeeres
 // @include      http://tagpro-*.koalabeast.com*
@@ -19,7 +19,7 @@ var spinJerseys = true;  // true or false
 // Add your own imgur album links here inside quotes with commas between quoted album links
 // For example: var customAlbums = ["http://imgur.com/a/0zBNw", "http://imgur.com/a/1abcD"]; (do not include comma if only 1 album)
 // Images should have titles that match team names and a single digit numerical description that matches team color (0 for either/both, 1 for red, 2 for blue)
-var Albums = ["https://imgur.com/a/tE24G", "https://imgur.com/a/fSicG", "https://imgur.com/a/yIgYZ"];
+var Albums = ["https://imgur.com/a/tE24G", "https://imgur.com/a/fSicG", "https://imgur.com/a/yIgYZ", "https://imgur.com/a/rX0GE", "https://imgur.com/a/ndsKa"];
 
 // Add your own imgur image links here inside quotes with commas between quoted image links, it must links to the png file
 // For example: var customImages = ["http://i.imgur.com/17aAwABc.png", "http://i.imgur.com/abc123DEF.png"]; (do not include comma if only 1 image)
@@ -104,7 +104,7 @@ function ObjectIndexOf(myArray, property, searchTerm) {  // searches for a prope
 
 function ajax_read_albums() {
     for (var a = 0; a < Albums.length; a++) {
-        var match = /([A-Za-z]+)\|([0-9])/;  // imgur description will be matched for this
+        var match = /([A-Za-z0-9_]+)\|([0-9])/;  // imgur description will be matched for this
         logd('Albums['+a+']: ' + Albums[a]);
         var id = Albums[a].match(/http[s]?:\/\/imgur\.com\/a\/(.+)[\/]?/)[1];  // [0] is the whole string, [1] only the matched group (.+);
         logd('id: ' + id);
@@ -120,6 +120,8 @@ function ajax_read_albums() {
                     if(curr.description && curr.title)
                     {
                         var descriptor = curr.description.match(match);
+                        logd('description: ' + curr.description);
+                        logd('description.match: ' + curr.description.match(match));
                         var league_index = ObjectIndexOf(data, "league", descriptor[1]);
                         if (league_index === -1)  // new league
                         {
@@ -519,122 +521,3 @@ else if (IAmIn === 'game') {  // ingame, draw jersey if there is one
         }
     });
 }
-
-/*!
- * jQuery Cookie Plugin v1.4.1
- * https://github.com/carhartl/jquery-cookie
- *
- * Copyright 2013 Klaus Hartl
- * Released under the MIT license
- */
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD
-		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
-		// CommonJS
-		factory(require('jquery'));
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
-}(function ($) {
-
-	var pluses = /\+/g;
-
-	function encode(s) {
-		return config.raw ? s : encodeURIComponent(s);
-	}
-
-	function decode(s) {
-		return config.raw ? s : decodeURIComponent(s);
-	}
-
-	function stringifyCookieValue(value) {
-		return encode(config.json ? JSON.stringify(value) : String(value));
-	}
-
-	function parseCookieValue(s) {
-		if (s.indexOf('"') === 0) {
-			// This is a quoted cookie as according to RFC2068, unescape...
-			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-		}
-
-		try {
-			// Replace server-side written pluses with spaces.
-			// If we can't decode the cookie, ignore it, it's unusable.
-			// If we can't parse the cookie, ignore it, it's unusable.
-			s = decodeURIComponent(s.replace(pluses, ' '));
-			return config.json ? JSON.parse(s) : s;
-		} catch(e) {}
-	}
-
-	function read(s, converter) {
-		var value = config.raw ? s : parseCookieValue(s);
-		return $.isFunction(converter) ? converter(value) : value;
-	}
-
-	var config = $.cookie = function (key, value, options) {
-
-		// Write
-
-		if (value !== undefined && !$.isFunction(value)) {
-			options = $.extend({}, config.defaults, options);
-
-			if (typeof options.expires === 'number') {
-				var days = options.expires, t = options.expires = new Date();
-				t.setTime(+t + days * 864e+5);
-			}
-
-			return (document.cookie = [
-				encode(key), '=', stringifyCookieValue(value),
-				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path    ? '; path=' + options.path : '',
-				options.domain  ? '; domain=' + options.domain : '',
-				options.secure  ? '; secure' : ''
-			].join(''));
-		}
-
-		// Read
-
-		var result = key ? undefined : {};
-
-		// To prevent the for loop in the first place assign an empty array
-		// in case there are no cookies at all. Also prevents odd result when
-		// calling $.cookie().
-		var cookies = document.cookie ? document.cookie.split('; ') : [];
-
-		for (var i = 0, l = cookies.length; i < l; i++) {
-			var parts = cookies[i].split('=');
-			var name = decode(parts.shift());
-			var cookie = parts.join('=');
-
-			if (key && key === name) {
-				// If second argument (value) is a function it's a converter...
-				result = read(cookie, value);
-				break;
-			}
-
-			// Prevent storing a cookie that we couldn't decode.
-			if (!key && (cookie = read(cookie)) !== undefined) {
-				result[name] = cookie;
-			}
-		}
-
-		return result;
-	};
-
-	config.defaults = {};
-
-	$.removeCookie = function (key, options) {
-		if ($.cookie(key) === undefined) {
-			return false;
-		}
-
-		// Must not alter options, thus extending a fresh object...
-		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
-		return !$.cookie(key);
-	};
-
-}));
-
